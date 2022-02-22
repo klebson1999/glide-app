4ecf3ce8fb0118ceb9616ebd73536fff5e5f2004
Clebson <klebson1999@gmail.com>
2022-02-21 23:02:45 -0300

    Fazendo cálculo

--- function.js
+++ function.js
@@ -5,14 +5,13 @@ window.function = function (inicial, final, retorno) {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-  inicial = moment().format(inicial.value ?? "");
-  final = moment().format(final.value ?? 0);
+  inicial = inicial.value ?? "";
+  final = final.value ?? 0;
   retorno = retorno.value ?? "";
-
+  resultado_final = "";
+  resultado_final = final.subtract(inicial);
 
   
 
-  
-
-  return retorno;
+  return resultado_final;
 }
