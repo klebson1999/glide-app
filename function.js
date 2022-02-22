09d05b30010a47d38ad85d09d8dddc40081ebfb6
Clebson <klebson1999@gmail.com>
2022-02-21 23:12:42 -0300

    Fazendo cálculo

--- function.js
+++ function.js
@@ -5,12 +5,12 @@ window.function = function (inicial, final, retorno) {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-  data = new Date(inicial);
+  data = new Date;
   final = final.value ?? "";
   retorno = retorno.value ?? "";
   resultado_final = data.subtract(10, 'days');
 
   
 
-  return resultado_final;
+  return console.log(resultado_final);
 }
