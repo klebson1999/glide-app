6cd97fa78fdb74bb76520bc5f957a038c0d422e1
Clebson <klebson1999@gmail.com>
2022-02-21 22:43:14 -0300

    Nome das variáveis

--- function.js
+++ function.js
@@ -1,15 +1,15 @@
 // A função aqui pega os parâmetros que você declarou no arquivo `glide.json`, na mesma ordem.
 
 
-window.function = function (data_inicial, data_final, formatacao_retorno) {
+window.function = function (inicial, final, retorno) {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-  data_inicial = moment().format(data_inicial.value ?? "");
-  data_final = moment().format(data_final.value ?? 0);
-  formatacao_retorno = formatacao_retorno.value;
+  data_inicial = moment().format(inicial.value ?? "");
+  data_final = moment().format(final.value ?? 0);
+  formatacao_retorno = retorno.value;
 
   
 
-  return str.substring(start, end);
+  return str.substring(retorno);
 }
