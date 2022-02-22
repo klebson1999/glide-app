e7c95a8e7caf38ad8f7ecd882567c9fe35a88ae1
Clebson <klebson1999@gmail.com>
2022-02-21 22:54:50 -0300

    Alteração nos arquivos

--- function.js
+++ function.js
@@ -5,11 +5,14 @@ window.function = function (inicial, final, retorno) {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-  data_inicial = moment().format(inicial.value ?? "");
-  data_final = moment().format(final.value ?? 0);
-  formatacao_retorno = retorno.value;
+  inicial = moment().format(inicial.value ?? "");
+  final = moment().format(final.value ?? 0);
+  retorno = retorno.value ?? "";
+
 
   
 
-  return str.substring(retorno);
+  
+
+  return retorno;
 }
