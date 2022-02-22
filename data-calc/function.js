e4505c8d2e52126baad802a29b69cb5287621d43
Clebson <klebson1999@gmail.com>
2022-02-21 22:18:33 -0300

    Adicionando Biblioteca Moment.js

--- data-calc/function.js
+++ data-calc/function.js
@@ -1,20 +1,15 @@
-// The function here takes the parameters that you
-// have declared in the `glide.json` file, in the
-// same order.
-window.function = function (str, start, end) {
-  // For each parameter, its `.value` contains
-  // either its value in the type you've declared,
-  // or it's `undefined`.  This is a good place to
-  // extract the `.value`s and assign default
-  // values.
-  str = str.value ?? "";
-  start = start.value ?? 0;
-  end = end.value;
+// A função aqui pega os parâmetros que você declarou no arquivo `glide.json`, na mesma ordem.
 
-  // Your function should return the exact type
-  // you've declared for the `result` in
-  // `glide.json`, or `undefined` if there's an
-  // error or no result can be produced, because a
-  // required input is `undefined`, for example.
+
+window.function = function (data_inicial, data_final, formatacao_retorno) {
+// Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
+// Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
+
+  data_inicial = moment().format(data_inicial.value ?? "");
+  data_final = moment().format(data_final.value ?? 0);
+  formatacao_retorno = formatacao_retorno.value;
+
+  
+
   return str.substring(start, end);
 }
