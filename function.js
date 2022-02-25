5280d4599c45897ac2902b36998e5648b052dd28
Clebson <klebson1999@gmail.com>
2022-02-24 21:55:39 -0300

    Atualização de 24/02/2022

--- function.js
+++ function.js
@@ -1,16 +1,17 @@
 // A função aqui pega os parâmetros que você declarou no arquivo `glide.json`, na mesma ordem.
 
 
-window.function = function (inicial, final, retorno) {
+function fun() {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-  data = new Date;
-  final = final.value ?? "";
-  retorno = retorno.value ?? "";
-  data.subtract(10, 'days');
+var data_inicial = new Date("02/22/2022"); 
+var data_futuro = new Date("10/05/2022");
 
-  
+var diferenca = Math.abs(data_futuro-data_inicial);
 
-  console.log(data);
+dias = diferenca/(1000 * 86400)
+semanas = diferenca/(1000 * 3600 * 24 * 7)
+meses = diferenca/(1000 * 3600 * 24 * 7)
+console.log(dias)
 }
