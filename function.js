50e49c935074f494ebee03baca7ac4737f70e571
Clebson <klebson1999@gmail.com>
2022-02-24 22:43:30 -0300

    Atualização de 24/02/2022

--- function.js
+++ function.js
@@ -5,8 +5,11 @@ window.function = function (inicial, final) {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-const vinicial = new Date(inicial); // Data de hoje
-const vfuture = new Date(final); // Outra data no passado
+var finicial = inicial.value ?? ""
+var ffinal = final.value ?? ""
+
+const vinicial = new Date(finicial); // Data de hoje
+const vfuture = new Date(); // Outra data no passado
 const diff = Math.abs(vfuture.getTime() - vinicial.getTime() ); // Subtrai uma data pela outra
 const mounth = Math.ceil(diff / (1000 * 60 * 60 * 24 * 30)); // Divide o total pelo total de milisegundos correspondentes a 1 dia. (1000 milisegundos = 1 segundo).
 
