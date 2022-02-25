675afdcb9e8af9e31d2d6d22a6cdbdce020c1d8f
Clebson <klebson1999@gmail.com>
2022-02-24 22:32:42 -0300

    Atualização de 24/02/2022

--- function.js
+++ function.js
@@ -1,17 +1,16 @@
 // A função aqui pega os parâmetros que você declarou no arquivo `glide.json`, na mesma ordem.
 
 
-function fun() {
+window.function = function (inicial, final, retorno) {
 // Para cada parâmetro, contém seu `.value` ou seu valor no tipo que você declarou, ou é `indefinido`.
 // Este é um bom lugar para extrai os `.value`s e atribui os valores padrão.
 
-var data_inicial = new Date("02/22/2022"); 
-var data_futuro = new Date("10/05/2022");
+const vinicial = new Date(inicial); // Data de hoje
+const vfuture = new Date(final); // Outra data no passado
+const diff = Math.abs(vfuture.getTime() - vinicial.getTime() ); // Subtrai uma data pela outra
+const mounth = Math.ceil(diff / (1000 * 60 * 60 * 24 * 30)); // Divide o total pelo total de milisegundos correspondentes a 1 dia. (1000 milisegundos = 1 segundo).
 
-var diferenca = Math.abs(data_futuro-data_inicial);
+// Mostra a diferença em dias
+return(mounth);
 
-dias = diferenca/(1000 * 86400)
-semanas = diferenca/(1000 * 3600 * 24 * 7)
-meses = diferenca/(1000 * 3600 * 24 * 7)
-console.log(dias)
 }
